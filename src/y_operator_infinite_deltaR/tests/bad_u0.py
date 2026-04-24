361196e753bc6395b3936d59082f665906851070
Moroz Nikita <moroz.na22@physics.msu.ru>
2026-04-24 13:41:04 +0300

    Deleted the redundunt folder 'y_operator_infinite_deltaR'

--- src/y_operator_infinite_deltaR/tests/bad_u0.py
+++
@@ -1,5 +0,0 @@
-from src.y_operator_deltaR.params import xi2, get_params
-from src.y_operator_deltaR.params import HBAR, M
-from src.y_operator_deltaR.construct_U0 import get_U, change_basis, get_U_deltaR
-import numpy as np
-
